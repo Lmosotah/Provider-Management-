a0174361cdbfc8bb47996fe8c8ec4439f6706f88
Lmosotah <lucy.mosotah17@gmail.com>
2026-05-19 22:07:00 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://bsxrbyjkkqahmlwgfeji.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzeHJieWpra3FhaG1sd2dmZWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTA0MzgsImV4cCI6MjA5NDc2NjQzOH0.u7V2ZFKeFo9d6UWQsPzNUZppumtUGor_oJYNLw_0zRs'
-)
